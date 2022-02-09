d3f3383b069cbb736cfc67d7de449fd2699000d9
OrbitalMechanist <pilot.max777@gmail.com>
2022-02-09 10:20:18 -0800

    "Changed skin and hair colors to a curated fantastic set."

--- Assets/Scripts/EmployeeGenerator.cs
+++ Assets/Scripts/EmployeeGenerator.cs
@@ -13,8 +13,10 @@ public class EmployeeGenerator : MonoBehaviour
     string[] femNames;
     string[] mNames;
 
-    public Color[] skinColors = {new Color(0.956f, 0.909f, 0.824f), new Color(0.878f, 0.780f, 0.616f), new Color(0.368f, 0.270f, 0.102f) };
-    public Color[] hairColors = {new Color(0.93f, 0.93f, 0.83f), new Color(0.666f, 0.545f, 0.278f), new Color(0.1f, 0.1f, 0.1f) };
+    public Color[] skinColors; 
+    //= {new Color(0.956f, 0.909f, 0.824f), new Color(0.878f, 0.780f, 0.616f), new Color(0.368f, 0.270f, 0.102f) };
+    public Color[] hatColors; 
+    //= {new Color(0.93f, 0.93f, 0.83f), new Color(0.666f, 0.545f, 0.278f), new Color(0.1f, 0.1f, 0.1f) };
 
     public Sprite[] shirts;
     public Sprite[] heads;
@@ -65,14 +67,14 @@ public class EmployeeGenerator : MonoBehaviour
         emp.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = shirts[Random.Range(0, shirts.Length)];
 
         //Head
-        emp.transform.GetChild(1).GetComponent<SpriteRenderer>().color = Random.ColorHSV(0, 1, 0.8f, 1f, 0.4f, 0.8f);
-        //skinColors[Random.Range(0, skinColors.Length)];
+        emp.transform.GetChild(1).GetComponent<SpriteRenderer>().color = skinColors[Random.Range(0, skinColors.Length)];
+        // Random.ColorHSV(0, 1, 0.8f, 1f, 0.4f, 0.8f);
         emp.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = heads[Random.Range(0, heads.Length)];
 
         //Hat
         emp.transform.GetChild(2).GetComponent<SpriteRenderer>().sprite = hats[Random.Range(0, hats.Length)];
-        emp.transform.GetChild(2).GetComponent<SpriteRenderer>().color = Random.ColorHSV(0, 1, 0f, 1f, 0.2f, 1f);
-        //hatColors[Random.Range(0, hairColors.Length)];
+        emp.transform.GetChild(2).GetComponent<SpriteRenderer>().color = hatColors[Random.Range(0, hatColors.Length)];
+        //Random.ColorHSV(0, 1, 0f, 1f, 0.2f, 1f);
 
         emp.GetComponent<Employee>().Create(fName, lName, personal, capability, ethic);
 
