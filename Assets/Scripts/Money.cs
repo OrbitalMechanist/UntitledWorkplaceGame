a2d95321dcf910b2323ce456c3410ab8ad4108df
CDGCodes <dpunter@my.bcit.ca>
2022-02-10 10:41:53 -0800

    Dynamic money

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+    public Text display;
+    public GameObject company;
+    // Update is called once per frame
+    void Update()
+    {
+        display.text = "$" + parseCash(company.GetComponent<Company>().cash);
+    }
+
+    string parseCash(int cash) {
+        if (cash>=1000) {
+            int higher = cash/1000;
+            int dif = cash-(1000*higher);
+            string stringDif = dif.ToString();
+            if (dif<100) {
+                if (dif<10) {
+                    stringDif = "0" + stringDif;
+                }
+                stringDif = "0" + stringDif;
+            }
+            return (parseCash(higher) + "," + stringDif);
+        } else {
+            return cash.ToString();
+        }
+    }
+}
--- Assets/Scripts/RandomEventTimer.cs
+++ Assets/Scripts/RandomEventTimer.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class RandomEventTimer : MonoBehaviour
 {
+    int count;
     public float time;
     public GameObject Event;
+    public GameObject company;
     // Start is called before the first frame update
     void Start()
     {
-        time = Random.Range(10.0f, 30.0f);
+        count = 0;
+        time = Random.Range(5.0f, 10.0f);
     }
 
     // Update is called once per frame
@@ -17,11 +20,24 @@ public class RandomEventTimer : MonoBehaviour
     {
         if (!Event.activeSelf) {
             if (time<=0.0f) {
+                count++;
                 time = Random.Range(5.0f, 10.0f);
                 Event.SetActive(true);
             } else {
                 time-=Time.deltaTime;
             }
         }
+    }
+
+    public void RaiseMoney()
+    {
+        company.GetComponent<Company>().cash+=100;
+        Event.SetActive(false);
+    }
+
+    public void LowerMoney()
+    {
+        company.GetComponent<Company>().cash-=100;
+        Event.SetActive(false);
     }
 }
