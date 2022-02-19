48fa2a6f5ae4ee6f70556d805dbf22b833f23521
CDGCodes <dpunter@my.bcit.ca>
2022-02-18 21:38:54 -0800

    Event Object created with modular button number count

--- Assets/Scripts/RandomEventTimer.cs
+++ Assets/Scripts/RandomEventTimer.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 
 public class RandomEventTimer : MonoBehaviour
 {
+    bool hasEvent = false;
     int count;
     int randEvent;
     UnityAction buttonCallBack;
     
-    public float time;
+    float time;
+    public Button button;
+    public GameObject canvas;
     public GameObject Event;
     public GameObject company;
     // Start is called before the first frame update
@@ -23,53 +26,43 @@ public class RandomEventTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Event.activeSelf) {
+        if (!hasEvent) {
             if (time<=0.0f) {
                 count++;
                 time = Random.Range(5.0f, 10.0f);
                 Event.SetActive(true);
-                randomizeEvents();
+                GameObject test = randomizeEvents();
+                test.transform.SetParent(canvas.transform, false);
+                hasEvent = true;
             } else {
                 time-=Time.deltaTime;
             }
         }
     }
 
-    public void randomizeEvents() {
-        Button ChoiceA = GameObject.Find("Choice A").GetComponent<Button>();
-        Button ChoiceB = GameObject.Find("Choice B").GetComponent<Button>();
-        Text desc = GameObject.Find("Desc").GetComponent<Text>();
-        ChoiceA.onClick.RemoveAllListeners();
-        ChoiceB.onClick.RemoveAllListeners();
-        randEvent = Random.Range(1, 4);
-        if (randEvent==1) {
-            desc.text = "Both buttons will raise money by $100";
-            ChoiceA.onClick.AddListener(delegate{RaiseMoney();});
-            ChoiceB.onClick.AddListener(delegate{RaiseMoney();});
-        } else if (randEvent==2) {
-            desc.text = "Both buttons will lower money by $100";
-            ChoiceA.onClick.AddListener(delegate{LowerMoney();});
-            ChoiceB.onClick.AddListener(delegate{LowerMoney();});
-        } else if (randEvent==3) {
-            desc.text = "Button A will raise money by $100, Button B will lower money by $100";
-            ChoiceA.onClick.AddListener(delegate{RaiseMoney();});
-            ChoiceB.onClick.AddListener(delegate{LowerMoney();});            
-        } else if (randEvent==4) {
-            desc.text = "Button A will lower money by $100, Button B will raise money by $100";
-            ChoiceA.onClick.AddListener(delegate{LowerMoney();});
-            ChoiceB.onClick.AddListener(delegate{RaiseMoney();});              
+    public GameObject randomizeEvents() {
+        GameObject newEvent = Instantiate(Event);
+        int buttonCount = Random.Range(0, 3);
+        for (int i = 0; i <= buttonCount; i++) {
+            Button newButton = Instantiate(button);
+            newButton.transform.SetParent(newEvent.transform, false);
+            newButton.transform.localPosition = new Vector3(0, -160+(i*50));
+            newButton.onClick.AddListener(delegate{closeEvent(newEvent);});
         }
+        return newEvent;
+    }
+    public void closeEvent(GameObject thisEvent) {
+        Destroy(thisEvent);
+        hasEvent = false;
     }
 
-    public void RaiseMoney()
+    public void RaiseMoney(int delta)
     {
-        company.GetComponent<Company>().cash+=100;
-        Event.SetActive(false);
+        company.GetComponent<Company>().cash+=delta;
     }
 
-    public void LowerMoney()
+    public void LowerMoney(int delta)
     {
-        company.GetComponent<Company>().cash-=100;
-        Event.SetActive(false);
+        company.GetComponent<Company>().cash-=delta;
     }
 }
