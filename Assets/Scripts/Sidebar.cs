20982f6fd9fc88cd0056c1a63b9e2c379d691269
Patrick Ng <png85@my.bcit.ca>
2022-03-23 09:35:51 -0700

    Added employee list side bar to PErforming stage (currently not populated)

--- Assets/Scripts/Sidebar.cs
+++ Assets/Scripts/Sidebar.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sidebar : MonoBehaviour
 
 {
     public GameObject Panel;
+    public Button panelButton;
     
+    public void Start() {
+        panelButton.onClick.AddListener(OpenPanel);
+    }
+
     public void OpenPanel()
     {
         Panel.SetActive(true);
+        panelButton.onClick.AddListener(ClosePanel);
+        panelButton.onClick.RemoveListener(OpenPanel);
     }
 
     public void ClosePanel()
     {
         Panel.SetActive(false);
+        panelButton.onClick.AddListener(OpenPanel);
+        panelButton.onClick.RemoveListener(ClosePanel);
     }
 }
