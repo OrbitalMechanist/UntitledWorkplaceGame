eca8b4a52946c4d55bdcd9bb44adc944bccfa441
CDGCodes <dpunter@my.bcit.ca>
2022-03-25 14:38:15 -0700

    Removed progress bar script

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -6,22 +6,14 @@ using UnityEngine.UI;
 
 public class Money : MonoBehaviour
 {
-    [SerializeField]
-    Text progressText;
-    [SerializeField]
-    RectTransform currentProgress;
     public Text moneyDisplay;
     public Text happyDisplay;
     public GameObject company;
-    private const int multiplyCount = 5;
-    private const int progressLength = 285;
     // Update is called once per frame
     void Update()
     {
         moneyDisplay.text = parseCash(company.GetComponent<Company>().cash);
         happyDisplay.text = parseCash(company.GetComponent<Company>().happiness);
-        progressText.text = "Progress: " + company.GetComponent<Company>().count * multiplyCount + "%";
-        setProgress(progressLength - (company.GetComponent<Company>().count * multiplyCount));
     }
 
     string parseCash(int cash) {
@@ -39,9 +31,5 @@ public class Money : MonoBehaviour
         } else {
             return cash.ToString();
         }
-    }
-
-    private void setProgress(float right) {
-        currentProgress.offsetMax = new Vector2(-right, currentProgress.offsetMax.y);
     }
 }
