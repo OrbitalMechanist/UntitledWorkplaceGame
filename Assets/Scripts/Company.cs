bf4fa766a37b6b2114907d9d4a278a24be8c93c0
CDGCodes <dpunter@my.bcit.ca>
2022-04-01 16:00:24 -0700

    Adjusted company balance

--- Assets/Scripts/Company.cs
+++ Assets/Scripts/Company.cs
@@ -11,7 +11,7 @@ public class Company : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cash = 100000;
+        cash = 10000;
         happiness = 100;
     }
 
