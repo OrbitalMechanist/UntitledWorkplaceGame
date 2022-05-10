2ec2b7a5660ef69603ead4eb0cbbed41df2b7614
Patrick Ng <png85@my.bcit.ca>
2022-05-09 21:31:38 -0700

    Fixed tooltips positioning not scaling correctly

--- Assets/Scripts/GenericUI/Tooltip.cs
+++ Assets/Scripts/GenericUI/Tooltip.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class Tooltip : MonoBehaviour
 {
     /** Horizontal offset of the tooltip relative to the mouse position. */
-    private const float offset = 15f;
+    private const float horizontalOffset = 15f;
 
     /** Camera of the UI. */
     public Camera uiCamera;
@@ -22,22 +22,46 @@ public class Tooltip : MonoBehaviour
     /** Rect of the tooltip. */
     private RectTransform rect;
 
+    /** The default width that the game was initially developed with. */
+    private const int defaultWidth = 800;
+
+    /** The default height that the game was initially developed with. */
+    private const int defaultHeight = 600;
+
+    /** The scale factor for the width of the tooltip. Needed to fix scaling issues on other resolutions due to manual position calculation. */
+    private float scaleWidth;
+
+    /** The scale factor for the height of the tooltip. Needed to fix scaling issues on other resolutions due to manual position calculation. */
+    private float scaleHeight;
+
     private void Awake() {
         // Get rect and text of tooltip
         rect =  this.gameObject.GetComponent<RectTransform>();
         tooltipText =  this.gameObject.GetComponentInChildren<Text>();
 
+        // Calculate the scaling required for the width and height values of the tooltip based on the current resolution
+        // i.e. 1600 x 1200 screen = 2x scaling required
+        scaleWidth = (float)uiCamera.pixelWidth / (float)defaultWidth;
+        scaleHeight = (float)uiCamera.pixelHeight / (float)defaultHeight;
+
         // Set min and max boundaries
         min = new Vector3(0, 0, 0);
-        max = new Vector3(uiCamera.pixelWidth, uiCamera.pixelHeight, 0);
+        max = new Vector3(uiCamera.pixelWidth, uiCamera.pixelHeight, 0); 
     }
 
     private void Update() {
+        // Calcualte the tooltip's scaled width and height
+        int scaledTooltipWidth = (int)(scaleWidth * rect.rect.width);
+        int scaledTooltipHeight = (int)(scaleHeight * rect.rect.height);
+
         // Get the tooltip position with offset
-        Vector3 position = new Vector3(Input.mousePosition.x + offset + rect.rect.width/2, Input.mousePosition.y - rect.rect.height, 0f);
+        Vector3 position = new Vector3(Input.mousePosition.x + horizontalOffset + (scaledTooltipWidth / 2), 
+                                       Input.mousePosition.y - scaledTooltipHeight, 0f);
 
         // Clamp it to the screen size so it doesn't go outside
-        this.gameObject.transform.position = new Vector3(Mathf.Clamp(position.x, min.x + rect.rect.width/2, max.x - rect.rect.width/2), Mathf.Clamp(position.y, min.y + rect.rect.height / 2, max.y - rect.rect.height / 2), transform.position.z);
+        this.gameObject.transform.position = new Vector3(Mathf.Clamp(position.x, min.x + (scaledTooltipWidth / 2), max.x - (scaledTooltipWidth / 2)), 
+                                                         Mathf.Clamp(position.y, min.y + (scaledTooltipHeight / 2), max.y - (scaledTooltipHeight / 2)), 
+                                                         transform.position.z);
     }
 
     public void ShowTooltip(string tooltipString) {
